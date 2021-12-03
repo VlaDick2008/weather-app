6fc9ad5f4bd288f050c3c85bc2eb1b54b0d8b7da
VlaDick2008 <sochukushat@yandex.ru>
2021-12-03 11:26:18 +0300

    Исправил ошибку с невозможностью изменить город, если получил данные по координатам. Узнал о useLayoutEffect, довольно полезный хук

--- src/App.js
+++ src/App.js
@@ -8,34 +8,57 @@ import WeatherDetails from './components/WeatherDetails';
 
 function App() {
   const [info, weatherData] = React.useState('');
-  const [sity, sityChange] = React.useState('Omsk');
+  const [city, cityChange] = React.useState('Омск');
+  const [position, positionChange] = React.useState('');
 
   const apiKey = '12cefab9977763ddc5f50d7e363a40f8';
   const apiBase = 'http://api.openweathermap.org/data/2.5/';
-  const apiCity = tr(sity);
+  const apiCity = tr(city);
 
   const updateCityData = (value) => {
-    sityChange(value);
+    cityChange(value);
+    console.log('обновил сити');
   };
 
-  console.log(apiCity);
+  const updatePositionData = (position) => {
+    positionChange(position);
+    console.log('обновил поситион');
+  };
+
+  React.useLayoutEffect(() => {
+    if (typeof position.coords !== 'undefined') {
+      fetch(
+        `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json&addressdetails=[0|1]`,
+      )
+        .then((resp) => resp.json())
+        .then((result) => {
+          cityChange(result.address.city);
+          console.log('определил координаты');
+        });
+    }
+  }, [position.coords]);
 
   React.useEffect(() => {
     fetch(`${apiBase}weather?q=${apiCity}&appid=${apiKey}&lang=ru&units=metric`)
       .then((resp) => resp.json())
       .then((result) => {
         weatherData(result);
+        console.log('получил город');
       });
   }, [apiCity]);
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     document.querySelector('body').classList.add('sunny');
   }, []);
 
   return (
     <div className="main-wrapper">
       <header>
-        <SityBlock updateCityData={updateCityData} />
+        <SityBlock
+          updatePositionData={updatePositionData}
+          cici={city}
+          updateCityData={updateCityData}
+        />
         <DegreeBlock items={['C', 'F']} />
       </header>
       <WeatherBlock weatherInfo={info} />
--- src/components/SityBlock.jsx
+++ src/components/SityBlock.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 
 import locationImg from '../img/location.svg';
 
-function SityBlock({ updateCityData }) {
+function SityBlock({ updateCityData, cici, updatePositionData }) {
   const changeSityBtn = React.useRef();
+  const [inputValue, setInputValue] = React.useState(cici);
 
   const success = (position) => {
-    console.log(position);
+    updatePositionData(position);
   };
 
-  const onButtonClick = () => {
+  const handleChange = () => {
+    setInputValue(changeSityBtn.value);
+  };
+
+  const removeDisabled = () => {
     changeSityBtn.current.removeAttribute('disabled');
   };
 
   return (
     <div className="sity-block">
       <input
-        defaultValue="Омск"
+        value={inputValue}
+        placeholder="Ваш город здесь..."
         type="text"
+        onChange={() => {
+          handleChange();
+        }}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             updateCityData(changeSityBtn.current.value);
@@ -29,13 +38,18 @@ function SityBlock({ updateCityData }) {
         itemID="sity-name"
       />
       <div className="change-sity-block">
-        <div onClick={onButtonClick} className="change-sity">
+        <div
+          onClick={() => {
+            removeDisabled();
+          }}
+          className="change-sity">
           Сменить город
         </div>
         <img className="position-logo" src={locationImg} alt="" />
         <div
           onClick={() => {
             navigator.geolocation.getCurrentPosition(success);
+            setInputValue(cici);
           }}
           className="your-position">
           Моё местоположение
